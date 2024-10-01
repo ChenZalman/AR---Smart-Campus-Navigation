16c8e009b908a9aace7e631ed29864a7c8617fd5
Chen <chenzalman96@gmail.com>
2024-10-01 13:37:59 +0300

    combining the pages into one project
    Added functionality to the pages and fixed some issues with layout. Need to check again for additional fixes and add more functionality

---
+++ src/pages/SignUpPage/index.jsx
@@ -0,0 +1,19 @@
+import { Helmet } from "react-helmet";
+import Footer from "../../components/Footer";
+import SignuppageTopviewpage from "./SignuppageTopviewpage";
+import React from "react";
+
+export default function SignUpPagePage() {
+  return (
+    <>
+      <Helmet>
+        <title>Signuppage</title>
+        <meta name="description" content="Web site created using create-react-app" />
+      </Helmet>
+      <div className="flex w-full flex-col gap-[46px] bg-white-a700">
+        {/* <SignuppageTopviewpage /> */}
+        <Footer />
+      </div>
+    </>
+  );
+}
