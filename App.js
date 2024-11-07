66617a7a2a295b9d8b03e7c8fc9009e9033d7f0d
OfirBuchacho <157514986+OfirBuchacho@users.noreply.github.com>
2024-11-07 18:09:35 +0200

    Avatar

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HebrewSpeaking3DAvatar from './HebrewSpeaking3DAvatar';
+
+function App() {
+  return (
+    <div className="App">
+      <HebrewSpeaking3DAvatar/>
+    </div>
+  );
+}
+
+export default App;
